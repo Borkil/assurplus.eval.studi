1b12f5c89d372e5bdf4812d2b899d340962e67a7
francois lalay <francois.lalay@gmail.com>
2023-03-06 18:14:23 +0100

    mise en place des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,31 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+
+use Faker\Factory;
+use App\Entity\Sinister;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    private $faker;
+
+    public function __construct() 
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    //'/^[a-zA_Z]{3}[-][0-9]{3}[-][a-zA_Z]{3}/'
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i=0; $i < 10; $i++) { 
+            $sinister = new Sinister();
+            $sinister->setAdressOfSinister($this->faker->Address())
+                ->setDescription($this->faker->Text())
+                ->setNumberRegistration('xx-123-xx');
+            $manager->persist($sinister);
+        }
 
         $manager->flush();
     }
