4ec8c48275c6f87a084c9a33c238e0859874beae
francois lalay <francois.lalay@gmail.com>
2023-03-16 09:41:09 +0100

    review code sinisterController

--- src/Controller/SinisterController.php
+++ src/Controller/SinisterController.php
@@ -70,32 +70,4 @@ class SinisterController extends AbstractController
         return $this->render('sinister/index.html.twig', ['form' => $form]);
     }
 
-    // #[Route('/declaration', name: 'form_sinister')]
-    // public function index(Request $request, EntityManagerInterface $entityManager): Response
-    // {
-    //     $sinister = new Sinister();
-
-    //     $form = $this->createForm(DeclarationSinisterType::class, $sinister );
-
-    //     $form->handleRequest($request);
-        
-    //     if ($form->isSubmitted() && $form->isValid())
-    //     {
-    //         $sinister = $form->getData();
-    //         $entityManager->persist($sinister);
-            
-    //         $entityManager->flush();
-            
-
-    //         $this->addFlash('success', 'Votre déclaration nous a bien été transmise');
-    //         return $this->redirectToRoute('app_home_page');
-
-    //     }else if($form->isSubmitted() && !$form->isValid())
-    //     {
-    //         $this->addFlash('danger', 'Il y a des erreurs dans la saisi du formulaire.');
-    //     }
-
-
-    //     return $this->render('sinister/index.html.twig', ['form' => $form]);
-    // }
 }
