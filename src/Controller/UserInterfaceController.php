fa2cc133d51eef3c765803beff5f2e2807fb5dea
francois lalay <francois.lalay@gmail.com>
2023-05-15 13:55:36 +0200

    add user interface page

---
+++ src/Controller/UserInterfaceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SinisterRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserInterfaceController extends AbstractController
+{
+    #[Route('/moncompte', name: 'app_user_historique')]
+    public function index(SinisterRepository $sinisterRepository): Response
+    {
+        return $this->render('user_interface/index.html.twig', [
+            'user' => $this->getUser(),
+            'sinisters' => $sinisterRepository->findAll()
+        ]);
+    }
+}
